020a6bc2eb879a9a309f1db49c8713655ecf9c6c
DevaZikrullah <zdeva804@gmail.com>
2022-04-26 19:20:57 +0700

    make cart model

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -2,36 +2,84 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Cart;
 use Illuminate\Http\Request;
 
 class CartController extends Controller
 {
-    public function getDataCart(){
-        $path = base_path() ."/mocking/Data_Cart.json";
-        $json = json_decode(file_get_contents($path),true);
-        return response()->json($json);
-        
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
     }
-    public function addDataCart(Request $request){
-        $path = base_path()."/mocking/cart.json";
-        $json = json_decode(file_get_contents($path),true);
-        array_push($json, $request->post());
-        return response()->json($json);
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
     }
-    // public function addDataCart(Request $request){
-    //     $bool = true;
-    //     $path = base_path()."/mocking/cart.json";
-    //     $json = json_decode(file_get_contents($path),true);
-    //     foreach ($json as $data){
-    //         if($data->id == $request->id){
-    //             $data->quantity += $request->quantity;
-    //             $bool = false;
-    //         }
-    //     }
-    //     if($bool){
-    //         array_push($json, $request->post());
-    //     }
-            
-    //     return response()->json($json);
-    // }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Cart  $cart
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Cart $cart)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Cart  $cart
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Cart $cart)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Cart  $cart
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Cart $cart)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Cart  $cart
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Cart $cart)
+    {
+        //
+    }
 }
--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -5,15 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Jenssegers\Mongodb\Eloquent\Model as Eloquent;
 
-class Item extends Eloquent
+class Cart extends Eloquent
 {
     use HasFactory;
-    
 
     protected $fillable = [
         'nama_item',
-        'desc',
-        'stock',
-        'harga'
+        'stock_beli'
     ];
 }
